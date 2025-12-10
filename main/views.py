48e4ba6292cf415f6fe324d740bde4aa4fe90b88
Dor04ka <Dor04ka@mail.ru>
2025-12-10 20:37:35 +0300

    Done Task2: models, migrations, admin setup + tasks list page

--- main/views.py
+++ main/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 def index(request):
     return HttpResponse("Привет, это мой первый Django-проект")
+
+def tasks_list(request):
+    tasks = Task.objects.all()
+    return render(request, "tasks_list.html", {"tasks": tasks})
 
 # Create your views here.
